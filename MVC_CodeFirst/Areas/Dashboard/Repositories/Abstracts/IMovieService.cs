f616811b4363af6c549a6585c2563f9f0753180f
kolijiva <talhasteam@hotmail.com>
2024-05-14 18:30:49 +0300

    Add project files.

---
+++ MVC_CodeFirst/Areas/Dashboard/Repositories/Abstracts/IMovieService.cs
@@ -0,0 +1,8 @@
+﻿using MVC_CodeFirst.Models.Entities;
+namespace MVC_CodeFirst.Areas.Dashboard.Repositories.Abstracts
+{
+    public interface IMovieService
+    {
+        void CreateMovie(Movie movie);
+    }
+}
---
+++ MVC_CodeFirst/Areas/Dashboard/Repositories/Services/MovieService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_CodeFirst.Areas.Dashboard.Repositories.Abstracts;
+using MVC_CodeFirst.Models.Context;
+using MVC_CodeFirst.Models.Entities;
+namespace MVC_CodeFirst.Areas.Dashboard.Repositories.Services
+{
+    public class MovieService : IMovieService
+    {
+        private MovieProjectContext _projectContext = new MovieProjectContext();
+        //public MovieService(MovieProjectContext movieProjectContext)
+        //{
+        //    _projectContext = movieProjectContext;
+        //}
+
+        public void CreateMovie(Movie movie)
+        {
+            try
+            {
+                _projectContext.Movies.Add(movie);
+                _projectContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
